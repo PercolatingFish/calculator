0a4e70aa89dd041c29abb2e188fa75871ca82249
PercolatingFish <robfarisvideography@gmail.com>
2023-02-13 10:46:33 -0500

    Make calculator math functional

--- scripts.js
+++ scripts.js
@@ -1,32 +1,65 @@
 const operators = {
-    add: function() {
+    'add': function() {
         let total = 0;
         for (num in arguments) {
             total += +arguments[num];
         }
         return total;
     },
-    subtract: function(num1, num2) {
+    'subtract': function(num1, num2) {
         return num1 - num2;
     },
-    multiply: function(num1, num2) {
+    'multiply': function(num1, num2) {
         return num1 * num2;
     },
-    divide: function(num1, num2) {
+    'divide': function(num1, num2) {
         return num1/num2;
     },
 }
 
 // create a new function operate that takes an operator and 2 numbers
 // and then calls one of the above functions on the numbers
-function operator (operator, num1, num2) {
+function operate (operator, num1, num2) {
     return operators[operator](num1, num2);
 }
 
-let displayInfo = ""
+let display = document.getElementById("display-id")
+
+let operator = null;
+let num1 = '';
+let num2 = '';
 
 function r(num) {
-    displayInfo += num;
-    display.textContent = displayInfo;}
+    // if (operator !== null) {
+    //     currentDisplay = "";
+    // }
+    num2 += num;
+    display.textContent = num2;
+}
 
-let display = document.getElementById("display-id")
+function b(operation) {
+    if (operator !== null) {
+        equals()
+    }
+    operator = operation;
+    num1 = num2;
+    num2 = '';
+}
+
+function textRemover() {
+    num1 = '';
+    num2 = '';
+    operator = null;
+    display.textContent = num2;
+    console.log("cleared")
+}
+
+
+function equals() {
+    if (operator == null) {
+        return
+    }
+    num2 = operate(operator, num1, num2);
+    display.textContent = num2;
+    operator = null;
+}
